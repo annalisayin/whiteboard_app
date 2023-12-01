40b16c71ff80ec051acf47a0b45572a576d52068
Edward Shao <edwardxzshao@gmail.com>
2023-11-30 22:56:58 -0500

    textbox model

---
+++ server/src/main/kotlin/models/TextBox.kt
@@ -0,0 +1,12 @@
+package models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class TextBox(
+    val offsetX: Int,
+    val offsetY: Int,
+    val curtext: String,
+    val color: Int,
+    val size: Int
+)
---
+++ server/src/main/kotlin/models/TextboxModel.kt
@@ -0,0 +1,21 @@
+package models
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.ResultRow
+
+object TBModel : IntIdTable() {
+    val offsetX = integer("offsetx")
+    val offsetY = integer("offsety")
+    val curtext = text("curtext")
+    val color = integer("color")
+    val size = integer("size")
+}
+
+fun ResultRow.toTextBox(): TextBox {
+    return TextBox(
+        this[TBModel.offsetX],
+        this[TBModel.offsetY],
+        this[TBModel.curtext],
+        this[TBModel.color],
+        this[TBModel.size]
+    )
+}
